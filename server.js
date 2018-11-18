411bdc59047ab23fb91c3a1bfef1d61c3e53e13c
Kralexe <you@example.com>
2018-11-18 10:41:56 +0300

    add

--- server.js
+++ server.js
@@ -32,22 +32,46 @@ io.sockets.on('connection', function(socket){
 		console.log('Disconnected: %s sockets connected', connections.length);
 	});
 
-	//on event 'send message' we emit data sent by user, his name and time of the day
-	socket.on('send message', function(data){
-		io.sockets.emit('new message', {msg:data, user: socket.username, hours:pad(new Date().getHours(), 2), minutes:pad(new Date().getMinutes(), 2)});
+	//on event 'send message' we print data sent by user, his name and time of the day. There's a built-in check if user entered some data or not, 
+	//in second case no input will be printed. There's no alert window, because it might disconcern some users
+		socket.on('send message', function(data){
+		if (data != ''){
+		io.sockets.emit('new message', {msg:data, user: socket.username, hours:pad(new Date().getHours(), 2), minutes:pad(new Date().getMinutes(), 2)});};
 	});
 
-	//when new user joined the caht his name is pushed to jquery variable username for displaying
+	//when new user joins, his name is pushed to jquery variable $username for displaying in scope of particular socket, if no name is entered in textfield then alert window
+	//will pop up. If nickname (case-insensitive) already taken, then another alert window will pop up urging user to change his nickname of choice. So, for example, if somebody
+	//already took username 'alex' then he won't be able to take username 'Alex' or '   aLex  '
 	socket.on('new user', function(data, callback){
-		callback(true);
+		let users_lower = users.map(item => item.toLowerCase().trim())
+		if(data != '' && !(users_lower.includes(data.toLowerCase().trim())))
+		{callback(true);
 		socket.username = data;
 		users.push(socket.username);
 		updateUsernames();
+		} else if (users_lower.includes(data.toLowerCase().trim())){
+			alertAnotherUser();
+		}
+		else {
+			alertUser();
+		}
 	});
 
 	//self-evident function related to Jquery function in index.html
 	function updateUsernames(){
 		io.sockets.emit('get users', users);
+	}
+
+	//emits alert window in index.html
+	function alertAnotherUser() {
+		//io.sockets.emit('alert user')
+		socket.emit('alert another user');
+	}
+
+	//emits alert window in index.html
+	function alertUser() {
+		//io.sockets.emit('alert user')
+		socket.emit('alert user');
 	}
 
 	//this function is required for padding new Date() methods with preceding zeros (just for pretty output)
